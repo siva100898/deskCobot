dce6f165a6cfe32e16097bec7d5b6f4f3da6cb80
Siva MAC <natarajansiva98@gmail.com>
2025-11-07 16:30:13 +0100

    Updated main.cpp : basic program with watchdog and elapsed timer tracker

--- software/src/lowerLevelControl/lowerLevelControl_main/src/main.cpp
+++ software/src/lowerLevelControl/lowerLevelControl_main/src/main.cpp
@@ -1,21 +1,55 @@
 #include <Arduino.h>
+#include <array>
+#include <cmath>
+#include <numeric>
+#include <algorithm>
+#include <optional>
+#include <chrono>
+#include <esp_timer.h>
 
-bool run ; 
+using namespace std::chrono;
+
+bool run;
+int counter;
+auto delay_mainCycle = 0ms;
+
+steady_clock::time_point lastCycle;
+constexpr auto watchDogThreshold = 50ms;
+
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    // Wait for Serial to be ready (especially for ESP32)
-    while (!Serial) {
-        delay(10);
-    }
-    run = true ; 
-    Serial.println("Setup is running!");
+  while (!Serial) {
+    delay(10);
+  }
+
+  run = true;
+  Serial.printf("C++ version macro: %ld\n", (long)__cplusplus);
+  Serial.println("Setup is running!");
 }
 
 void loop() {
-  if (run){
-    Serial.println("Loop is running...");
-    run = false ; 
+  auto startCycle = steady_clock::now();
+
+  // -------- Main section --------
+  counter = 0;
+  while (counter < 50) {
+    counter = counter + 1;
   }
-  delay(1000);
+
+  delay(delay_mainCycle.count());  // convert chrono::duration to integer milliseconds
+  delay_mainCycle += 1ms;
+
+  auto endCycle = steady_clock::now();
+  auto timeElapsed = endCycle - startCycle;
+  auto timeElapsed_ms = duration_cast<milliseconds>(timeElapsed);
+
+  
+
+  if (timeElapsed > watchDogThreshold) {
+    Serial.printf("[UTILITY] Time elapsed : %lld ms\n",
+      static_cast<long long>(timeElapsed_ms.count()));
+    Serial.println("[WATCHDOG] Watchdog breach!");
+    delay_mainCycle = 0ms;  // ✅ properly resets
+  }
 }
